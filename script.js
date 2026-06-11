f7304770c42b87a76bd1f012f99a9723b8f9ca37
targaryen007 <39377020+targaryen007@users.noreply.github.com>
2026-06-11 13:15:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function createHeart(){
+const heart=document.createElement("div");
+heart.className="heart";
+heart.innerHTML="❤️";
 
+heart.style.left=Math.random()*100+"vw";
+heart.style.animationDuration=
+(5+Math.random()*5)+"s";
+
+document.querySelector(".hearts").appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},10000);
+}
+
+setInterval(createHeart,500);
